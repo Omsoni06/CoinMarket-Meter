a73d9bd6cff67c86434825ca9e6e8d2b228b8f73
om soni <omsoni774600@gmail.com>
2024-03-12 16:37:18 +0500

    app name changed

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -41,7 +41,7 @@ function Banner() {
               fontFamily: "Montserrat",
             }}
           >
-            Crypto Tracker
+            CoinMarket Meter
           </Typography>
           <Typography
             variant="subtitle2"
